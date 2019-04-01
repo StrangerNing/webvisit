813f55f1df6f7f2c8961c7fd12f5472d00be6ce6
朱泽宁 <tsening.cn@gmail.com>
2019-04-02 00:11:35 +0800

    引入redis

--- src/main/java/com/webvisit/WebvisitApplication.java
+++ src/main/java/com/webvisit/WebvisitApplication.java
@@ -3,7 +3,9 @@ package com.webvisit;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @MapperScan("com.webvisit.dao")
 public class WebvisitApplication {
