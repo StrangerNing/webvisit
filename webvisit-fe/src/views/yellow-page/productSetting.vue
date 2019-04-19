9c9fb135ec44acd8651564a3e82030545cc64ee3
朱泽宁 <tsening.cn@gmail.com>
2019-04-20 01:47:22 +0800

    封装上传图片

--- webvisit-fe/src/views/yellow-page/productSetting.vue
+++ webvisit-fe/src/views/yellow-page/productSetting.vue
@@ -67,13 +67,13 @@
         >
           <el-form :model="product" label-width="25%" style="width: 100%;">
             <el-form-item label="产品名称：">
-              <el-input v-model="product.name" style="width: 80%;" />
+              <el-input v-model="product.name" style="width: 80%;"/>
             </el-form-item>
             <el-form-item label="产品链接：">
-              <el-input v-model="product.productUrl" style="width: 80%;" />
+              <el-input v-model="product.productUrl" style="width: 80%;"/>
             </el-form-item>
             <el-form-item label="产品详情：">
-              <el-input v-model="product.detail" style="width: 80%;" type="textarea" rows="3" />
+              <el-input v-model="product.detail" style="width: 80%;" type="textarea" rows="3"/>
             </el-form-item>
             <el-form-item v-if="product.id" label="编辑产品图片：">
               <el-button icon="el-icon-edit" @click="editCompanyImg()">编辑</el-button>
@@ -109,12 +109,13 @@
                       <img :src="scope.row.imgUrl" style="width: 145px;height: 145px;">
                     </span>
                     <span v-else>
-                      <i class="el-icon-plus" />
+                      <i class="el-icon-plus"/>
                     </span>
                   </div>
                 </span>
                 <span v-else>
-                  <img :src="scope.row.imgUrl" style="width: 145px;height: 145px;cursor: pointer" @click="showImage(scope.row.imgUrl)">
+                  <img :src="scope.row.imgUrl" style="width: 145px;height: 145px;cursor: pointer"
+                       @click="showImage(scope.row.imgUrl)">
                 </span>
               </span>
             </el-table-column>
@@ -124,7 +125,7 @@
             >
               <span slot-scope="scope">
                 <span v-if="editProductImg">
-                  <el-input v-model="scope.row.imgDetail" type="textarea" rows="6" />
+                  <el-input v-model="scope.row.imgDetail" type="textarea" rows="6"/>
                 </span>
                 <span v-else>
                   {{ scope.row.imgDetail }}
@@ -135,19 +136,31 @@
               label="操作"
             >
               <span slot-scope="scope">
-                <span v-if="editProductImg">
-                  <el-button
-                    type="primary"
-                    icon="el-icon-picture"
-                    @click="chooseProductImg(scope.$index, scope.row)"
-                  >选择图片
-                    <input id="upload_file" type="file" style="display: none" @change="photoChange($event)"></input></el-button>
-                  <el-button
-                    type="primary"
-                    icon="el-icon-upload"
-                    @click="submitProductImg(scope.$index, scope.row)"
-                  >提交</el-button>
-                </span>
+                <el-row>
+                  <div v-if="editProductImg">
+                    <el-button
+                      type="primary"
+                      icon="el-icon-picture"
+                      @click="chooseProductImg(scope.$index, scope.row)"
+                    >选择图片
+                      <input :id="upload_file + scope.$index" type="file" style="display: none"
+                             @change="photoChange($event)"></input>
+                    </el-button>
+                    <el-button
+                      type="primary"
+                      icon="el-icon-upload"
+                      @click="submitProductImg(scope.$index, scope.row)"
+                    >提交</el-button>
+                  </div>
+                  <div v-else>
+                    <el-button
+                      type="danger"
+                      size="small"
+                      icon="el-icon-delete"
+                      @click="deleteProductImg(scope.$index, scope.row)"
+                    >删除</el-button>
+                  </div>
+                </el-row>
               </span>
             </el-table-column>
           </el-table>
@@ -161,225 +174,206 @@
 </template>
 
 <script>
-import {
-  addCompanyProduct, addProductImg,
-  deleteCompanyProduct,
-  editCompanyProduct,
-  getCompanyProduct,
-  uploadApi as yellowPage, uploadImg
-} from '../../api/yellowPage'
-import { Message } from 'element-ui'
+  import {
+    addCompanyProduct, addProductImg,
+    deleteCompanyProduct,
+    editCompanyProduct,
+    getCompanyProduct, updateProductImg,
+    uploadApi as yellowPage, uploadImg
+  } from '../../api/yellowPage'
+  import { Message } from 'element-ui'
 
-export default {
-  name: 'ProductSetting',
-  data() {
-    return {
-      saveImg: yellowPage.saveImg,
-      productList: [],
-      editProductVisible: false,
-      editProductTitle: '编辑产品信息',
-      editProductImgVisible: false,
-      editProductImg: false,
-      dialogImgVisible: false,
-      dialogImageUrl: '',
-      product: {
-        id: null,
-        companyId: null,
-        name: '',
-        detail: '',
-        productUrl: '',
-        productImgList: []
-      },
-      productImgList: [],
-      productImg: {
-        id: null,
-        productId: null,
-        imgUrl: '',
-        imgDetail: ''
-      },
-      custom: ''
-    }
-  },
-  created() {
-    this.getProductList()
-  },
-  methods: {
-    getProductList() {
-      getCompanyProduct().then(res => {
-        this.productList = res.data
-      })
-    },
-    newProduct() {
-      this.editProductTitle = '新增产品'
-      for (const key in this.product) {
-        this.product[key] = null
-      }
-      this.editProductVisible = true
-    },
-    commitProduct() {
-      if (this.product.id == null) {
-        addCompanyProduct(this.product).then(res => {
-          this.checkCommitProduct(res)
-        })
-      } else {
-        editCompanyProduct(this.product).then(res => {
-          this.checkCommitProduct(res)
-        })
-      }
-    },
-    deleteProduct(index, row) {
-      this.$confirm('被删除的产品无法恢复, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        deleteCompanyProduct({ productId: row.id }).then(res => {
-          if (res.data) {
-            Message({
-              message: '删除成功',
-              type: 'success',
-              duration: 10 * 1000
-            })
-            this.getProductList()
-          } else {
-            Message({
-              message: '删除失败',
-              type: 'error',
-              duration: 10 * 1000
-            })
-          }
-        })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '已取消删除'
-        })
-      })
-    },
-    checkCommitProduct(res) {
-      if (res.data) {
-        Message({
-          message: '提交成功',
-          type: 'success',
-          duration: 10 * 1000
-        })
-        this.editProductVisible = false
-        this.getProductList()
-      } else {
-        Message({
-          message: '提交失败',
-          type: 'error',
-          duration: 10 * 1000
-        })
-      }
-    },
-    editProduct(index, row) {
-      this.editProductTitle = '编辑产品信息'
-      for (const key in row) {
-        this.product[key] = row[key]
-      }
-      this.editProductVisible = true
-    },
-    editCompanyImg() {
-      this.editProductVisible = false
-      this.editProductImgVisible = true
-      this.productImgList = []
-      this.editProductImg = false
-      for (const item in this.product.productImgList) {
-        this.productImgList[item] = this.product.productImgList[item]
-      }
-    },
-    addProductImg() {
-      const productImg = {
-        id: null,
-        productId: this.product.id,
-        imgUrl: null,
-        imgDetail: null
-      }
-      this.productImgList.push(productImg)
-    },
-    editProductImage() {
-      this.editProductImg = true
-    },
-    submitProductImg(index, row) {
-      if (row.id == null) {
-        addProductImg(row).then(res => {
-          if (res.data) {
-            Message({
-              message: '提交成功',
-              type: 'success',
-              duration: 10 * 1000
-            })
-            this.getProductList()
-          } else {
-            Message({
-              message: '提交失败',
-              type: 'error',
-              duration: 10 * 1000
-            })
-          }
-        })
-      }
-    },
-    saveImage: function(param) {
-      const formData = new FormData()
-      formData.append('file', param.file)
-      formData.append('type', 'SKU')
-      console.log(param)
-      uploadImg(formData).then(res => {
-        this.productImg.imgUrl = res.data
-      })
-    },
-    showImage(url) {
-      this.dialogImageUrl = url
-      this.dialogImgVisible = true
-    },
-    photoChange(el) {
-      const file = el.target.files[0]
-      const type = file.type.split('/')[0]
-      if (type === 'image') {
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        const that = this
-        reader.onloadend = function() {
-          const dataURL = reader.result
-          const blob = that.dataURItoBlob(dataURL)
-          that.upload(blob)
+  export default {
+    name: 'ProductSetting',
+    data() {
+      return {
+        saveImg: yellowPage.saveImg,
+        productList: [],
+        editProductVisible: false,
+        editProductTitle: '编辑产品信息',
+        editProductImgVisible: false,
+        editProductImg: false,
+        dialogImgVisible: false,
+        dialogImageUrl: '',
+        upload_file: 'upload_file',
+        product: {
+          id: null,
+          companyId: null,
+          name: '',
+          detail: '',
+          productUrl: '',
+          productImgList: []
+        },
+        productImgList: [],
+        productImg: {
+          id: null,
+          productId: null,
+          imgUrl: '',
+          imgDetail: ''
+        },
+        temp: {
+          index: null,
+          row: null
         }
-      } else {
-        Message({
-          message: '请重新选择图片',
-          type: 'error',
-          duration: 10 * 1000
-        })
       }
     },
-    chooseProductImg() {
-      document.getElementById('upload_file').click()
+    created() {
+      this.getProductList()
     },
-    dataURItoBlob(dataURI) {
-      const byteString = window.atob(dataURI.split(',')[1])
-      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-      const T = mimeString.split('/')[1]
-      const ab = new ArrayBuffer(byteString.length)
-      const ia = new Uint8Array(ab)
-      for (let i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i)
-      }
-      return new Blob([ab], { type: mimeString })
-    },
-    upload(imgUrl) {
-      const formData = new FormData()
-      formData.append('file', imgUrl)
-      uploadImg(formData).then(res => {
-        Message({
-          message: '上传成功',
-          type: 'success'
+    methods: {
+      getProductList() {
+        getCompanyProduct().then(res => {
+          this.productList = res.data
         })
-      })
+      },
+      newProduct() {
+        this.editProductTitle = '新增产品'
+        for (const key in this.product) {
+          this.product[key] = null
+        }
+        this.editProductVisible = true
+      },
+      commitProduct() {
+        if (this.product.id == null) {
+          addCompanyProduct(this.product).then(res => {
+            this.checkCommitProduct(res)
+          })
+        } else {
+          editCompanyProduct(this.product).then(res => {
+            this.checkCommitProduct(res)
+          })
+        }
+      },
+      deleteProduct(index, row) {
+        this.$confirm('被删除的产品无法恢复, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          deleteCompanyProduct({ productId: row.id }).then(res => {
+            if (res.data) {
+              Message({
+                message: '删除成功',
+                type: 'success',
+                duration: 10 * 1000
+              })
+              this.getProductList()
+            } else {
+              Message({
+                message: '删除失败',
+                type: 'error',
+                duration: 10 * 1000
+              })
+            }
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+      },
+      checkCommitProduct(res) {
+        if (res.data) {
+          Message({
+            message: '提交成功',
+            type: 'success',
+            duration: 10 * 1000
+          })
+          this.editProductVisible = false
+          this.getProductList()
+        } else {
+          Message({
+            message: '提交失败',
+            type: 'error',
+            duration: 10 * 1000
+          })
+        }
+      },
+      editProduct(index, row) {
+        this.editProductTitle = '编辑产品信息'
+        for (const key in row) {
+          this.product[key] = row[key]
+        }
+        this.productImgList.splice(0,this.productImgList.length)
+        console.log(this.productImgList)
+        for (const item in this.productList[index].productImgList) {
+          console.log('重置')
+          this.productImgList.push(this.productList[index].productImgList[item])
+        }
+        console.log(this.productImgList)
+        this.editProductVisible = true
+      },
+      editCompanyImg() {
+        this.editProductVisible = false
+        this.editProductImgVisible = true
+        this.editProductImg = false
+      },
+      addProductImg() {
+        const productImg = {
+          id: null,
+          productId: this.product.id,
+          imgUrl: null,
+          imgDetail: null
+        }
+        this.productImgList.push(productImg)
+      },
+      editProductImage() {
+        this.editProductImg = true
+      },
+      submitProductImg(index, row) {
+        console.log(index,row)
+        if (row.id == null) {
+          addProductImg(row).then(res => {
+            this.checkSubmitImg(res)
+          })
+        } else {
+          updateProductImg(row).then(res => {
+            this.checkSubmitImg(res)
+          })
+        }
+      },
+      checkSubmitImg(res) {
+        if (res.data) {
+          Message({
+            message: '提交成功',
+            type: 'success',
+            duration: 10 * 1000
+          })
+          this.getProductList()
+        } else {
+          Message({
+            message: '提交失败',
+            type: 'error',
+            duration: 10 * 1000
+          })
+        }
+      },
+      showImage(url) {
+        this.dialogImageUrl = url
+        this.dialogImgVisible = true
+      },
+      photoChange(el) {
+        const file = el.target.files[0]
+        const formData = new FormData()
+        formData.append('file', file)
+        uploadImg(formData).then(res => {
+          this.temp.row.imgUrl = res.data
+          Message({
+            message: '上传成功',
+            type: 'success'
+          })
+        })
+      },
+      chooseProductImg(index, row) {
+        this.temp.index = index
+        this.temp.row = row
+        let id = this.upload_file+index
+        console.log(index, row)
+        document.getElementById(id).click()
+      }
     }
   }
-}
 </script>
 
 <style scoped>
