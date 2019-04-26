eab8f4118b337dfc74f48be525272414b27b47c0
朱泽宁 <zening.zhu@ucarinc.com>
2019-04-26 17:59:42 +0800

    加入quartz定时任务（未完成）

---
+++ webvisit-os/src/main/java/com/webvisit/quartz/QuartzFactory.java
@@ -0,0 +1,20 @@
+package com.webvisit.quartz;
+
+import com.webvisit.utils.SpringUtil;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * @author zening.zhu
+ * @version 1.0
+ * @date 2019/4/26
+ */
+
+public class QuartzFactory implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
---
+++ webvisit-os/src/main/java/com/webvisit/utils/SpringUtil.java
@@ -0,0 +1,30 @@
+package com.webvisit.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zening.zhu
+ * @version 1.0
+ * @date 2019/4/26
+ */
+
+@Component
+public class SpringUtil implements BeanFactoryPostProcessor {
+
+    private static ConfigurableListableBeanFactory beanFactory;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringUtil.beanFactory = beanFactory;
+    }
+
+    public static Object getBean(String name) {
+        return beanFactory.getBean(name);
+    }
+    public static <T> T getBean(Class<T> clazz){
+        return beanFactory.getBean(clazz);
+    }
+}
